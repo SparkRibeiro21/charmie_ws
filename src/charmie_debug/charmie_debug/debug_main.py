f50b0fe9885c827812bcf16afaa0f701bb81f10a
SparkRibeiro21 <tiagoribeiro80@gmail.com>
2023-05-31 16:04:32 +0100

    Added Get and Flag Topics to the Neck Node

--- src/charmie_debug/charmie_debug/debug_main.py
+++ src/charmie_debug/charmie_debug/debug_main.py
@@ -3,123 +3,77 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Pose2D 
+from std_msgs.msg import Bool
 import time
+
+flag_get_neck_position = False 
 
 class TRNode(Node):
 
     def __init__(self):
         super().__init__("Debug")
         self.get_logger().info("Initialised CHARMIE Debug Node")
-        self.neck_position_subscriber = self.create_subscription(Pose2D, "neck_pos", self.neck_position_callback , 10)
-        self.neck_coordinates_publisher = self.create_publisher(Pose2D, "neck_coord", 10)
+        self.neck_position_publisher = self.create_publisher(Pose2D, "neck_pos", 10)
         self.neck_error_publisher = self.create_publisher(Pose2D, "neck_error", 10)
-        self.counter = 0
+        self.neck_get_position_subscriber = self.create_subscription(Pose2D, "get_neck_pos", self.get_neck_position_callback , 10)
+        self.flag_neck_position_publisher = self.create_publisher(Bool, "flag_neck_pos", 10)
 
+        self.counter = 1 # starts at 1 to avoid initial 
         self.create_timer(5, self.timer_callback)
 
-    def neck_position_callback(self, pos: Pose2D):
+    def get_neck_position_callback(self, pos: Pose2D):
         print("Received: pan =", int(pos.x), " tilt = ", int(pos.y))
 
     def timer_callback(self):
         cmd = Pose2D()
+        flag = Bool()
+        global flag_get_neck_position
 
-        cmd.x = 180.0
-        cmd.y = 180.0 
-        self.neck_coordinates_publisher.publish(cmd)
-        time.sleep(2)
-
-        cmd.x = 60.0
-        cmd.y = 60.0 
-        self.neck_error_publisher.publish(cmd)
-        time.sleep(0.05)
-
-        cmd.x = 60.0
-        cmd.y = 60.0 
-        self.neck_error_publisher.publish(cmd)
-        time.sleep(0.05)
-
-        cmd.x = 60.0
-        cmd.y = 60.0 
-        self.neck_error_publisher.publish(cmd)
-        time.sleep(0.05)
-
-        cmd.x = 60.0
-        cmd.y = 60.0 
-        self.neck_error_publisher.publish(cmd)
-        time.sleep(0.05)
-
-        cmd.x = 60.0
-        cmd.y = 60.0 
-        self.neck_error_publisher.publish(cmd)
-        time.sleep(0.05)
-
-        cmd.x = 60.0
-        cmd.y = 60.0 
-        self.neck_error_publisher.publish(cmd)
-        time.sleep(0.05)
-
-        cmd.x = 60.0
-        cmd.y = 60.0 
-        self.neck_error_publisher.publish(cmd)
-        time.sleep(0.05)
-
-        cmd.x = 60.0
-        cmd.y = 60.0 
-        self.neck_error_publisher.publish(cmd)
-        time.sleep(0.05)
-
-        cmd.x = 60.0
-        cmd.y = 60.0 
-        self.neck_error_publisher.publish(cmd)
-        time.sleep(0.05)
-
-        cmd.x = 60.0
-        cmd.y = 60.0 
-        self.neck_error_publisher.publish(cmd)
-        time.sleep(0.05)
-
-        """
         if self.counter == 0:
             cmd.x = 180.0
             cmd.y = 180.0 
+            flag_get_neck_position = False
         if self.counter == 1:
-            cmd.x = 300.0
+            cmd.x = 270.0
             cmd.y = 180.0 
         if self.counter == 2:
             cmd.x = 180.0
             cmd.y = 180.0 
+            flag_get_neck_position = True
         if self.counter == 3:
-            cmd.x = 60.0
+            cmd.x = 90.0
             cmd.y = 180.0 
         if self.counter == 4:
             cmd.x = 180.0
             cmd.y = 180.0 
+            flag_get_neck_position = False
         if self.counter == 5:
             cmd.x = 180.0
             cmd.y = 120.0 
         if self.counter == 6:
             cmd.x = 180.0
             cmd.y = 180.0 
+            flag_get_neck_position = True
         if self.counter == 7:
             cmd.x = 180.0
-            cmd.y = 270.0 
+            cmd.y = 235.0 
             self.counter = -1
-        
-        # k = cv2.waitKey(1)
-        # if k == ord('a'):
-        # if k == ord('s'):
-        # cmd.x = 3000
-        # cmd.y = 3000
-        
-        # cmd.x = 2000.0
-        # cmd.y = 2000.0
 
-        self.neck_coordinates_publisher.publish(cmd)
+        self.neck_position_publisher.publish(cmd)
+        flag.data = flag_get_neck_position
+        self.flag_neck_position_publisher.publish(flag)
+        print("DATA SENT ", self.counter)
         self.counter+=1
-        """
 
 def main(args=None):
     rclpy.init(args=args)
     node = TRNode()
+
+    cmd = Pose2D()
+    cmd.x = 180.0
+    cmd.y = 180.0
+    node.neck_position_publisher.publish(cmd)
+    print("INITIAL STATE")
+
     rclpy.spin(node)
     rclpy.shutdown()
--- src/charmie_neck_dynamixel/charmie_neck_dynamixel/neck_dynamixel.py
+++ src/charmie_neck_dynamixel/charmie_neck_dynamixel/neck_dynamixel.py
@@ -3,6 +3,7 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Pose2D 
+from std_msgs.msg import Bool
 
 import tty
 import termios
@@ -105,19 +106,14 @@ class NeckNode(Node):
     def __init__(self):
         super().__init__("Neck")
         self.get_logger().info("Initialised CHARMIE Neck Node")
-        self.neck_coordinates_subscriber = self.create_subscription(Pose2D, "neck_coord", self.neck_coordinates_callback ,10)
+        self.neck_position_subscriber = self.create_subscription(Pose2D, "neck_pos", self.neck_position_callback ,10)
         self.neck_error_subscriber = self.create_subscription(Pose2D, "neck_error", self.neck_error_callback , 10)
-        self.neck_position_publisher = self.create_publisher(Pose2D, "neck_pos", 10)
+        self.neck_get_position_publisher = self.create_publisher(Pose2D, "get_neck_pos", 10)
+        self.flag_neck_position_subscriber = self.create_subscription(Bool, "flag_neck_pos", self.flag_neck_position_callback , 10)
 
-        self.create_timer(0.5, self.timer_position_callback)
         self.create_timer(0.1, self.timer_callback)
-
-    def timer_position_callback(self):
-        cmd = Pose2D()
-        cmd.x = pan_corr * SERVO_TICKS_TO_DEGREES_CONST
-        cmd.y = tilt_corr * SERVO_TICKS_TO_DEGREES_CONST
-        
-        self.neck_position_publisher.publish(cmd)
+        self.flag_get_neck_position = False
+        self.k_e = 0.5
 
 
     def timer_callback(self):
@@ -126,37 +122,44 @@ class NeckNode(Node):
         
         global pan_corr, tilt_corr
 
+        # when the servo is moving it usually returns values extremely high, this way these values are filtered
         if pan < 4096:
             pan_corr = pan
         if tilt < 4096:
             tilt_corr = tilt
         
-        # print("PAN = " + str(pan) + " TILT = " + str(tilt) + "PAN_F = " + str(pan_corr) + " TILT_F = " + str(tilt_corr))
         # print("PAN = ", pan, " TILT = ", tilt, " PAN_F = ", pan_corr, " TILT_F = ", tilt_corr)
-        print("PAN_F = ", int(pan_corr * SERVO_TICKS_TO_DEGREES_CONST + 0.5), " TILT_F = ", int(tilt_corr * SERVO_TICKS_TO_DEGREES_CONST + 0.5))
+        # print("PAN_F = ", int(pan_corr * SERVO_TICKS_TO_DEGREES_CONST + 0.5), " TILT_F = ", int(tilt_corr * SERVO_TICKS_TO_DEGREES_CONST + 0.5))
 
-    def neck_coordinates_callback(self, coords: Pose2D):
+        # print( self.flag_get_neck_position)
+        if  self.flag_get_neck_position:
+            cmd = Pose2D()
+            cmd.x = pan_corr * SERVO_TICKS_TO_DEGREES_CONST
+            cmd.y = tilt_corr * SERVO_TICKS_TO_DEGREES_CONST
+            self.neck_get_position_publisher.publish(cmd)
 
-        # pan = int(coords.x * 11.3777292 + 0.5)
-        # tilt = int(coords.y * 11.3777292 + 0.5)
 
-        print("Received: pan =", coords.x, " tilt = ", coords.y)
-        # print("Converted: pan =", pan, " tilt = ", tilt)
-
+    def neck_position_callback(self, coords: Pose2D):
+        self.get_logger().info("Received Neck Position, Adjusting the Neck Position")
+        # print("Received Position: pan =", coords.x, " tilt = ", coords.y)
         move_neck(coords.x, coords.y)
 
 
     def neck_error_callback(self, error: Pose2D):
-        print(error)
-        k_e = 0.5
-        print(pan_corr)
-
-        move_neck((pan_corr * SERVO_TICKS_TO_DEGREES_CONST) - (error.x*k_e), (tilt_corr * SERVO_TICKS_TO_DEGREES_CONST) - (error.y*k_e))
-        pass
-
+        self.get_logger().info("Received Neck Position (by Pixel Errors), Adjusting the Neck Position")
+        move_neck((pan_corr * SERVO_TICKS_TO_DEGREES_CONST) - (error.x*self.k_e), (tilt_corr * SERVO_TICKS_TO_DEGREES_CONST) - (error.y*self.k_e))
+        
+    def flag_neck_position_callback(self, flag: Bool):
+        # print("Flag Neck Position Set To: ", flag.data)
+        if flag.data:
+            self.get_logger().info("Received Reading Neck Position State True")
+        else:
+            self.get_logger().info("Received Reading Neck Position State False")
+        self.flag_get_neck_position = flag.data
+        
 
 def move_neck(p, t):
-    print(p,t)
+    # print(p,t)
 
     if p > MAX_PAN_ANGLE:
         p = MAX_PAN_ANGLE
@@ -186,6 +189,7 @@ def move_neck(p, t):
 
 def main(args=None):
     rclpy.init(args=args)
+    node = NeckNode()
 
     # Open port
     if portHandler.openPort():
@@ -205,7 +209,6 @@ def main(args=None):
         getch()
         quit()
 
-
     # Enable Dynamixel Torque
     dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID_TILT, ADDR_MX_TORQUE_ENABLE,
                                                               TORQUE_ENABLE)
@@ -223,9 +226,9 @@ def main(args=None):
         print("PAN %s" % packetHandler.getRxPacketError(dxl_error))
     else:
         print("Dynamixel PAN has been successfully connected")
+    node.get_logger().info("Set Torque Mode")
 
     orig_settings = termios.tcgetattr(sys.stdin)
-
     tty.setcbreak(sys.stdin)
 
     dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID_PAN, ADDR_MX_D_GAIN, PAN_D_GAIN)
@@ -236,16 +239,8 @@ def main(args=None):
     dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID_TILT, ADDR_MX_I_GAIN, TILT_I_GAIN)
     dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID_TILT, ADDR_MX_P_GAIN, TILT_P_GAIN)
 
+    move_neck(180, 180) # resets the neck whenever the node is started, so that at the beginning the neck is always facing forward 
+    node.get_logger().info("Set Neck to Initial Position, Looking Forward")
 
-    # pan, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(portHandler, DXL_ID_PAN, ADDR_MX_PRESENT_POSITION)
-    # tilt, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(portHandler, DXL_ID_TILT, ADDR_MX_PRESENT_POSITION)
-
-    move_neck(180, 180)
-
-    tilt_move = 0
-    pan_move = 0
-
-
-    node = NeckNode()
     rclpy.spin(node)
     rclpy.shutdown()
