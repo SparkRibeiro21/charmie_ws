e8e74395655a3d307e49e5705e8452bef874bf20
TiagoRibeiro <tiagoribeiro80@gmail.com>
2023-10-23 10:22:48 +0100

    Added debug_visual pkg

---
+++ src/charmie_debug_visual/charmie_debug_visual/debug_visual.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import Bool, String, Float32
+from charmie_interfaces.msg import SpeechType, RobotSpeech
+
+class Robot():
+    def __init__(self):
+        print("New Robot Class Initialised")
+
+    # your code here
+
+class RobotNode(Node):
+
+    def __init__(self):
+        super().__init__("Robot")
+        self.get_logger().info("Initialised CHARMIE Debug Visual Node")
+
+        self.speaker_publisher = self.create_publisher(RobotSpeech, "speech_command", 10)        
+        self.flag_speaker_subscriber = self.create_subscription(Bool, "flag_speech_done", self.get_speech_done_callback, 10)
+        
+        self.robot = Robot()
+
+    def get_speech_done_callback(self, state: Bool):
+        print("Received Speech Flag:", state.data)
+        self.get_logger().info("Received Speech Flag")
+
+    
+def main(args=None):
+    rclpy.init(args=args)
+    node = RobotNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
