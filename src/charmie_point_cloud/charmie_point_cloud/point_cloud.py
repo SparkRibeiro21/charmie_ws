cc3150be0f689e268bf7fe5d6933b2d87fbb8afc
TiagoRibeiro <tiagoribeiro80@gmail.com>
2023-10-27 15:01:41 +0100

    Created Point Cloud Variables

--- src/charmie_point_cloud/charmie_point_cloud/point_cloud.py
+++ src/charmie_point_cloud/charmie_point_cloud/point_cloud.py
@@ -2,33 +2,52 @@
 import rclpy
 from rclpy.node import Node
 
+from sensor_msgs.msg import Image
 from std_msgs.msg import Bool, String, Float32
-from charmie_interfaces.msg import SpeechType, RobotSpeech
+from charmie_interfaces.msg import SpeechType, RobotSpeech, NeckPosition, RequestPointCloud, RetrievePointCloud
 
-class Robot():
+class PointCloud():
     def __init__(self):
-        print("New Robot Class Initialised")
+        print("New PointCloud Class Initialised")
 
     # your code here
 
-class RobotNode(Node):
+class PointCloudNode(Node):
 
     def __init__(self):
-        super().__init__("Robot")
-        self.get_logger().info("Initialised CHARMIE Robot Node")
-
-        self.speaker_publisher = self.create_publisher(RobotSpeech, "speech_command", 10)        
-        self.flag_speaker_subscriber = self.create_subscription(Bool, "flag_speech_done", self.get_speech_done_callback, 10)
+        super().__init__("PointCloud")
+        self.get_logger().info("Initialised CHARMIE PointCloud Node")
         
-        self.robot = Robot()
+        # Intel Realsense Subscribers
+        self.color_image_subscriber = self.create_subscription(Image, "/color/image_raw", self.get_color_image_callback, 10)
+        self.aligned_depth_image_subscriber = self.create_subscription(Image, "/aligned_depth_to_color/image_raw", self.get_aligned_depth_image_callback, 10)
+        
+        # Neck Position
+        self.neck_get_position_subscriber = self.create_subscription(NeckPosition, "get_neck_pos", self.get_neck_position_callback, 10)
 
-    def get_speech_done_callback(self, state: Bool):
-        print("Received Speech Flag:", state.data)
-        self.get_logger().info("Received Speech Flag")
+        # RequestPointCloud
+        self.request_point_cloud_subscriber = self.create_subscription(RequestPointCloud, "get_point_cloud", self.get_request_point_cloud_callback, 10)
 
+        # RetrievePointCloud
+        self.retrieve_point_cloud_publisher = self.create_publisher(RetrievePointCloud, "set_point_cloud", 10)
+
+
+        self.pcloud = PointCloud()
+
+    def get_color_image_callback(self, img: Image):
+        pass
+
+    def get_aligned_depth_image_callback(self, img: Image):
+        pass
+
+    def get_neck_position_callback(self, neck_pos: NeckPosition):
+        pass
+
+    def get_request_point_cloud_callback(self, req: RequestPointCloud):
+        pass
     
 def main(args=None):
     rclpy.init(args=args)
-    node = RobotNode()
+    node = PointCloudNode()
     rclpy.spin(node)
     rclpy.shutdown()
