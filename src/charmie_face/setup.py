99a1f32c8a4dbfc8c40c0963308de4bf7d59cde4
SparkRibeiro21 <tiagoribeiro80@gmail.com>
2024-02-05 19:11:54 +0000

    Restructured face packages for new tablet face

--- src/charmie_face/setup.py
+++ src/charmie_face/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-package_name = 'charmie_face_shiningrgb'
+package_name = 'charmie_face'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "face_shiningrgb = charmie_face_shiningrgb.face_shiningrgb:main"
+		"face = charmie_face.face:main"
         ],
     },
 )
