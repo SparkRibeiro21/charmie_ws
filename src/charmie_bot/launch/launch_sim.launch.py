2e9c4f6aa800ef242270ea281de8763d2ad4d3af
Bruno <brunogdpa7@gmail.com>
2024-02-16 11:23:41 +0000

    Added the most recent URDF file with the chassis, speakers, power box and torso

--- src/charmie_bot/launch/launch_sim.launch.py
+++ src/charmie_bot/launch/launch_sim.launch.py
@@ -3,9 +3,10 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, RegisterEventHandler
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.conditions import UnlessCondition
+from launch.substitutions import LaunchConfiguration, ThisLaunchFileDir
 from launch_ros.substitutions import FindPackageShare
 
 from launch_ros.actions import Node
@@ -17,7 +18,7 @@ import xacro
 def generate_launch_description():
 
     package_name='charmie_bot' #<--- CHANGE ME
-
+    
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
@@ -58,6 +59,7 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
+        #arm_rviz_launch,
         gazebo,
         spawn_entity,
         joint_state 
