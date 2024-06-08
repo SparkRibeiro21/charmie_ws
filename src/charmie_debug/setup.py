0403554ac8f05c1bac3219439d9288430b4c3ed0
SparkRibeiro21 <tiagoribeiro80@gmail.com>
2024-06-08 23:00:46 +0100

    added new debug obstacles to start obstacle fusion between two lidars and head camera

--- src/charmie_debug/setup.py
+++ src/charmie_debug/setup.py
@@ -29,6 +29,7 @@ setup(
             "debug_inspection_person_detection = charmie_debug.debug_inspection_person_detection:main",
             "debug_main = charmie_debug.debug_main:main",
             "debug_navigation_localization = charmie_debug.debug_navigation_localization:main",
+            "debug_obstacles = charmie_debug.debug_obstacles:main",
             "debug_pick_place_all_objects = charmie_debug.debug_pick_place_all_objects:main",
             "debug_search_for_person = charmie_debug.debug_search_for_person:main",
 		    "debug_spkr_face = charmie_debug.debug_spkr_face:main",
