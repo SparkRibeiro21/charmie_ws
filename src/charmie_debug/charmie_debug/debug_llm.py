36ef6e8cd1efb496af65909a37613647ca4ac504
Gbeca <gbeca104@gmail.com>
2024-11-19 16:57:58 +0000

    Removed test paragraphs again

--- src/charmie_debug/charmie_debug/debug_llm.py
+++ src/charmie_debug/charmie_debug/debug_llm.py
@@ -31,7 +31,6 @@ ros2_modules = {
     "charmie_yolo_pose":        False,
 }
 
-
 # main function that already creates the thread for the task state machine
 def main(args=None):
     rclpy.init(args=args)
