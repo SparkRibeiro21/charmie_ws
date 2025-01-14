a85ee67566f502c2d4eadf83e8103b23f7812de0
Tiago Ribeiro <tiagoribeiro80@gmail.com>
2025-01-14 09:45:48 +0000

    Added launch files for robot_state and joint_state

---
+++ src/charmie_description/launch/display.launch.py
@@ -0,0 +1,36 @@
+
+from launch import LaunchDescription
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_path
+
+def generate_launch_description():
+
+    urdf_path = os.path.join(get_package_share_path('charmie_description'), 
+                             'urdf', 'charmie_.urdf')
+
+    robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{'robot_description': robot_description}]
+    )
+
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui"
+    )
+
+    rviz2_node = Node(
+        package="rviz2",
+        executable="rviz2"
+    )
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        joint_state_publisher_node,
+        rviz2_node
+    ])
