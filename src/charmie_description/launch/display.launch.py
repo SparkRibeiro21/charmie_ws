6b2483b7f8b0ae402834d441685f5b26a29ad7f1
Tiago Ribeiro <tiagoribeiro80@gmail.com>
2025-02-19 11:34:44 +0000

    Revert "Added in real robot torso and neck"
    This reverts commit f3eabca4a22b435e0d3ae8279c98694d89370a28.

--- src/charmie_description/launch/display.launch.py
+++ src/charmie_description/launch/display.launch.py
@@ -2,16 +2,10 @@ from launch import LaunchDescription
 from launch_ros.parameter_descriptions import ParameterValue
 from launch.substitutions import Command
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription, TimerAction
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
 from ament_index_python.packages import get_package_share_path
 
-from charmie_std_functions.launch_std_functions import LaunchStdFunctions
-
 def generate_launch_description():
-
-    std_lf = LaunchStdFunctions() # From charmie_std_functions - Standardizes launch files
 
     urdf_path = os.path.join(get_package_share_path('charmie_description'), 
                              'urdf', 'charmie_real.urdf.xacro')
@@ -38,81 +32,8 @@ def generate_launch_description():
         arguments=['-d', rviz_config_path]
     )
 
-    # not used in real robot (so had to be manually added)
-    static_transform_left_wheel = Node( 
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='static_transform_left_wheel',
-        arguments=[
-            '0',  # x
-            '0.255',  # y
-            '0.05',  # z
-            '0',  # roll
-            '0',  # pitch
-            '0',  # yaw
-            'base_link',  # parent frame
-            'left_wheel_link'  # child frame
-        ]
-    )
-
-    # not used in real robot (so had to be manually added)
-    static_transform_right_wheel = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='static_transform_right_wheel',
-        arguments=[
-            '0',  # x
-            '-0.255',  # y
-            '0.05',  # z
-            '0',  # roll
-            '0',  # pitch
-            '0',  # yaw
-            'base_link',  # parent frame
-            'right_wheel_link'  # child frame
-        ]
-    )
-
-    # not implemented yet, we assume everytime we need TFs the robot legs and torso are on top position
-    static_transform_torso_legs = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='static_transform_right_wheel',
-        arguments=[
-            '0',  # x
-            '0',  # y
-            '0.475',  # z
-            '0',  # roll
-            '0',  # pitch
-            '0',  # yaw
-            'base_link',  # parent frame
-            'legs_link'  # child frame
-        ]
-    )
-
-    # not implemented yet, we assume everytime we need TFs the robot legs and torso are on top position
-    static_transform_torso_torso = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='static_transform_right_wheel',
-        arguments=[
-            '0',  # x
-            '0',  # y
-            '0.25',  # z
-            '0',  # roll
-            '0.15708',  # pitch
-            '0',  # yaw
-            'legs_link',  # parent frame
-            'torso_link'  # child frame
-        ]
-    )
-
-
     return LaunchDescription([
         robot_state_publisher_node,
-        # joint_state_publisher_node,
-        static_transform_left_wheel,
-        static_transform_right_wheel,
-        static_transform_torso_legs,
-        static_transform_torso_torso,
+        joint_state_publisher_node,
         rviz2_node
     ])
