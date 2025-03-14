54cbf65fe2fa85fcdad98f96af5918ff43d5beb9
Tiago Ribeiro <tiagoribeiro80@gmail.com>
2025-03-14 16:15:09 +0000

    Finished actions tests. Added accept/reject goal, set goal succeeded/aborted/canceled, and different goal management policies.

--- src/charmie_debug/charmie_debug/test_actions_client.py
+++ src/charmie_debug/charmie_debug/test_actions_client.py
@@ -3,8 +3,10 @@ import rclpy
 import rclpy.action
 from rclpy.node import Node
 from rclpy.action import ActionClient
-from rclpy.action.client import ClientGoalHandle
+from rclpy.action.client import ClientGoalHandle, GoalStatus
 from charmie_interfaces.action import CountUntil
+
+import time
 
 
 class CountUntilClientNode(Node):
@@ -28,7 +30,15 @@ class CountUntilClientNode(Node):
         
         ### send_goal blocks the ros2 structure whereas the send_goal_async does not
         # self.count_until_client_.send_goal(goal)
-        self.count_until_client_.send_goal_async(goal).add_done_callback(self.goal_response_callback)
+        self.count_until_client_.send_goal_async(goal, feedback_callback=self.goal_feedback_callback).add_done_callback(self.goal_response_callback)
+
+        # Send a cancel request 2 seconds later
+        # self.timer_ = self.create_timer(2.0, self.cancel_goal)
+
+    def cancel_goal(self):
+        self.get_logger().info("Canceling goal...")
+        self.goal_handle_.cancel_goal_async() # Not ideal, but just to test, goal_handle_ is only defined in goal_response_callback
+        self.timer_.cancel()
 
     def goal_response_callback(self, future):
         
@@ -41,15 +51,26 @@ class CountUntilClientNode(Node):
 
 
     def goal_result_callback(self, future):
+        status = future.result().status
         result = future.result().result
+
+        if status == GoalStatus.STATUS_SUCCEEDED:
+            self.get_logger().info("SUCCEEDED.")
+        elif status == GoalStatus.STATUS_ABORTED:
+            self.get_logger().error("ABORTED.")
+        elif status == GoalStatus.STATUS_CANCELED:
+            self.get_logger().warn("CANCELED.")
+            
         self.get_logger().info(f"Result: {result.reached_number}")
 
-
+    def goal_feedback_callback(self, feedback_msg):
+        number = feedback_msg.feedback.current_number
+        self.get_logger().info(f"Feedback: {number}")
 
 def main(args=None):
     rclpy.init(args=args)
     node = CountUntilClientNode()
-    node.send_goal(-6, 1.0)
+    node.send_goal(20, 1.0)
     rclpy.spin(node)
     rclpy.shutdown()
 
--- src/charmie_debug/charmie_debug/test_actions_server.py
+++ src/charmie_debug/charmie_debug/test_actions_server.py
@@ -1,35 +1,77 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from rclpy.action import ActionServer, GoalResponse
+from rclpy.action import ActionServer, GoalResponse, CancelResponse
 from rclpy.action.server import ServerGoalHandle
 from charmie_interfaces.action import CountUntil
 
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.callback_groups import ReentrantCallbackGroup
+
 import time
+import threading
 
 
 class CountUntilServerNode(Node):
     def __init__(self):
         super().__init__("count_until_server")
+        self.goal_handle_: ServerGoalHandle = None
+        self.goal_lock = threading.Lock()
+        self.goal_queue = []
         self.count_until_server_ = ActionServer(
             self, 
             CountUntil, 
             "count_until",
             goal_callback=self.goal_callback,
-            execute_callback=self.execute_callback)
+            handle_accepted_callback=self.handle_accepted_callback,
+            cancel_callback=self.cancel_callback,
+            execute_callback=self.execute_callback,
+            callback_group=ReentrantCallbackGroup()
+            )
         self.get_logger().info("Action Server has been started.")
 
     def goal_callback(self, goal_request: CountUntil.Goal):
         self.get_logger().info("Received new goal request.")
 
+        # Policy: refuse new goal if current goal still active
+        # with self.goal_lock:
+        #     if self.goal_handle_ is not None and self.goal_handle_.is_active:
+        #         self.get_logger().info("Another goal is already active. Rejecting new goal")
+        #         return GoalResponse.REJECT
+
         # Validate the goal request
         if goal_request.target_number <= 0:
             self.get_logger().warn("Invalid target number.")
             return GoalResponse.REJECT
+        
+        # Policy: preempt existing goal when receiving new goal
+        # with self.goal_lock:
+        #     if self.goal_handle_ is not None and self.goal_handle_.is_active:
+        #         self.get_logger().info("Abborting current goal and accepting new goal.")
+        #         self.goal_handle_.abort()
+
         self.get_logger().info("Accepted the goal.")
         return GoalResponse.ACCEPT
 
+    def handle_accepted_callback(self, goal_handle: ServerGoalHandle):
+        with self.goal_lock:
+            if self.goal_handle_ is not None:
+                self.goal_queue.append(goal_handle)
+
+            else:
+                goal_handle.execute()
+
+
+
+    def cancel_callback(self, goal_handle: ServerGoalHandle):
+        self.get_logger().warn("Received cancel request.")
+        return CancelResponse.ACCEPT
+        # return CancelResponse.REJECT
+
     def execute_callback(self, goal_handle: ServerGoalHandle):
+        
+        with self.goal_lock:
+            self.goal_handle_ = goal_handle
         
         # Get request from goal
         target_number = goal_handle.request.target_number
@@ -37,40 +79,47 @@ class CountUntilServerNode(Node):
 
         # Execute the action
         self.get_logger().info("Executing goal...")
+        feedback = CountUntil.Feedback()
+        result = CountUntil.Result()
         counter = 0
         for i in range(target_number):
+            if not goal_handle.is_active:
+                self.get_logger().warn("Goal has been abborted.")
+                result.reached_number = counter
+                self.process_next_goal_in_queue()
+                return result
+            if goal_handle.is_cancel_requested:
+                goal_handle.canceled()
+                self.get_logger().warn("Goal has been canceled.")
+                result.reached_number = counter
+                self.process_next_goal_in_queue()
+                return result
             counter += 1
             self.get_logger().info(f"Current number: {counter}")
+            feedback.current_number = counter
+            goal_handle.publish_feedback(feedback)
             time.sleep(period)
 
         # Once done, set the goal final state
         goal_handle.succeed()
+        # goal_handle.abort()
 
         # And send the result
-        result = CountUntil.Result()
         result.reached_number = counter
+        self.process_next_goal_in_queue()
         return result 
-
-
-
-        """
-        if goal_handle.is_cancel_requested:
-            goal_handle.canceled()
-            self.get_logger().info("Goal has been canceled.")
-            return CountUntil.Result()
-        counter += 1
-        goal_handle.publish_feedback(CountUntil.Feedback(current_number=counter))
-        self.get_logger().info(f"Current number: {counter}")
-        self.get_logger().info(f"Period: {period}")
-        self.get_logger().info(f"Target number: {target_number}")
-        self.get_logger().info("")
-        """
-
+    
+    def process_next_goal_in_queue(self):
+        with self.goal_lock:
+            if len(self.goal_queue) > 0:
+                self.goal_queue.pop(0).execute()
+            else:
+                self.goal_handle_ = None
 
 def main(args=None):
     rclpy.init(args=args)
     node = CountUntilServerNode()
-    rclpy.spin(node)
+    rclpy.spin(node, MultiThreadedExecutor())
     rclpy.shutdown()
 
 
