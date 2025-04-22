78611f60ed6303c02509882bc73eef6ee448f3a9
Tiago Ribeiro <tiagoribeiro80@gmail.com>
2025-04-23 00:19:35 +0200

    Added if clause for face version in URDF and example launch file for URDF, default is version 2. Already done for when the update for face version 3 is ON.

--- src/charmie_description/launch/example_arg_parameters_urdf_launch.py
+++ src/charmie_description/launch/example_arg_parameters_urdf_launch.py
@@ -10,9 +10,11 @@ from launch_ros.substitutions import FindPackageShare
 def generate_launch_description():
     # Declare launch arguments
     wheel_radius_arg = DeclareLaunchArgument('wheel_radius', default_value='0.1')
+    head_version_arg = DeclareLaunchArgument('head_version', default_value='1')
     
     # Use launch configuration variables
     wheel_radius = LaunchConfiguration('wheel_radius')
+    head_version = LaunchConfiguration('head_version')
 
     # Path to the xacro file
     xacro_file = PathJoinSubstitution([
@@ -24,16 +26,20 @@ def generate_launch_description():
     # Generate robot_description from xacro with parameters
     robot_description_content = Command([
         'xacro ', xacro_file,
-        ' wheel_radius:=', wheel_radius
+        ' wheel_radius:=', wheel_radius,
+        ' head_version:=', head_version
     ])
 
     robot_description = {'robot_description': robot_description_content}
 
     return LaunchDescription([
         wheel_radius_arg,
+        head_version_arg,
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             parameters=[robot_description]
         )
     ])
+
+
