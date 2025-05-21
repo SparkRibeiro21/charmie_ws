605fe3b9d3a9f5bda2e1f929d41b467071841ae3
Tiago Ribeiro <tiagoribeiro80@gmail.com>
2025-05-22 00:16:00 +0100

    Updated serve breakfast task to final version. Most significant change was updating navigation to nav2 std_functions.

--- src/charmie_serve_breakfast/launch/serve_breakfast_launch.launch.py
+++ src/charmie_serve_breakfast/launch/serve_breakfast_launch.launch.py
@@ -6,29 +6,34 @@ def generate_launch_description():
     std_lf = LaunchStdFunctions() # From charmie_std_functions - Standardizes launch files
     
     return LaunchDescription([
-        
-        ###std_lf.gui,
-        # std_lf.audio,
+        std_lf.robot_state_publisher_real_node,
+        std_lf.static_transforms_launch,
+        std_lf.rviz2_nav2_node,
+        std_lf.gui,
+        std_lf.marker_arrays_debug,
+
         LaunchDescription(std_lf.declared_arm_arguments + [std_lf.robot_arm_driver_launch]),
         std_lf.arm,
-        ###std_lf.speakers,
-        ###std_lf.charmie_multi_camera_launch_description,
-        ###std_lf.charmie_orbbec_base_camera_launch_description,
-        # std_lf.low_level,
+        
+        std_lf.low_level,
+        std_lf.lidar,
+        std_lf.lidar_bottom,
+        
+        std_lf.speakers,
+        std_lf.neck,
         ###std_lf.face,
-        ###std_lf.neck,
+        
+        std_lf.odometry_lidar,
+        std_lf.robot_localization,
+        std_lf.charmie_localisation,
 
-        # std_lf.ps4_controller,
-        ###std_lf.yolo_objects,
-        # std_lf.yolo_pose,
-        # std_lf.lidar,
-        # std_lf.odometry_lidar,
-        # std_lf.robot_localization
-        # std_lf.llm
-        
-        ### std_lf.obstacles,
-        ### std_lf.navigation
-        
-        # std_lf.node_robot_state_publisher,
-        # std_lf.joint_state,
+        std_lf.delayed_nav2_localization,        
+        std_lf.nav2_navigation,
+
+        # CAMS have to be after nav2 otherwise map does not show up in rviz
+        std_lf.charmie_multi_camera_launch_description,
+        std_lf.charmie_orbbec_base_camera_launch_description,
+
+        std_lf.yolo_objects,
+
     ])
