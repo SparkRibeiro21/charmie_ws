aa9de55bddf7191c1ccbb0ab74c8bdcf10f4bc72
Tiago Ribeiro <tiagoribeiro80@gmail.com>
2025-09-21 19:03:19 +0100

    Created charmie_tasks package. A new package for all tasks (including RoboCup) rather than each having their own pkg.

---
+++ src/charmie_tasks/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'charmie_tasks'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tiago',
+    maintainer_email='tiagoribeiro80@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
