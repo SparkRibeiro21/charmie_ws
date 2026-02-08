10a2e871061dcdb2be0b98d486ec81aaf47e37be
Tiago Ribeiro <tiagoribeiro80@gmail.com>
2026-02-08 20:29:06 +0000

    Added charmie_yolo_world pkg

---
+++ src/charmie_yolo_world/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'charmie_yolo_world'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tiago',
+    maintainer_email='tiagoribeiro80@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
