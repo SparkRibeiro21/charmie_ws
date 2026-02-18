66027007e8773c1e515df679bde9bd4407659868
Tiago Ribeiro <tiagoribeiro80@gmail.com>
2026-02-18 16:12:10 +0000

    Updates to charmie_sdnl_nav pkg structure

--- src/charmie_sdnl_nav/launch/sdnl_nav_launch.py
+++ src/charmie_sdnl_nav/launch/sdnl_nav_launch.py
@@ -4,14 +4,18 @@ from ament_index_python.packages import get_package_share_path
 import os
 
 def generate_launch_description():
+    sdnl_params_yaml_path = os.path.join(
+        get_package_share_path('charmie_description'),
+        'config',
+        'sdnl_nav_params.yaml'
+    )
+
     return LaunchDescription([
         Node(
             package='charmie_sdnl_nav',
             executable='sdnl_nav',
             name='sdnl_nav',
             output='screen',
-            parameters=[{
-                'sdnl_nav_config': os.path.join(get_package_share_path('charmie_description'), 'config', 'sdnl_nav_params.yaml')
-            }]
+            parameters=[sdnl_params_yaml_path]
         )
     ])
