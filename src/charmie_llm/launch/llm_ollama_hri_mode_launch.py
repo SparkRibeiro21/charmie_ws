83539a24c99898904b0d0880c3b332d9d541769d
Tiago Ribeiro <tiagoribeiro80@gmail.com>
2026-04-17 21:34:29 +0100

    Added specific launch files for 3 llm task modes

---
+++ src/charmie_llm/launch/llm_ollama_hri_mode_launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+
+    return LaunchDescription([
+        Node(
+            package='charmie_llm',
+            executable='llm_ollama',
+            name='llm_ollama',
+            output='screen',
+            emulate_tty=True,
+            parameters=[{
+                'task': 'hri',
+            }]
+        )
+    ])
