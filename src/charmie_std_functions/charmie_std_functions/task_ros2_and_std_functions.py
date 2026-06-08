d9d9024f1ab732b2004c6c45a10ec6ed7a22b4aa
Tiago Ribeiro <tiagoribeiro80@gmail.com>
2026-06-08 21:12:36 +0100

    Added rotation to move_to_person when DetectedPerson has a furniture_location (mostly used for furnitures with multiple sitting positions)

--- src/charmie_std_functions/charmie_std_functions/task_ros2_and_std_functions.py
+++ src/charmie_std_functions/charmie_std_functions/task_ros2_and_std_functions.py
@@ -2650,13 +2650,13 @@ class RobotStdFunctions():
         
     def move_to_person(self, person=DetectedPerson(), dist_to_person=2.0, print_feedback=True, feedback_freq=1.0, wait_for_end_of=True):
 
+        move_coords = [person.position_absolute.x, person.position_absolute.y, 0.0]
+
         if person.furniture_location == "None" or person.furniture_location == None: # if the person is not in a specific furniture, assume we can navigate there and have generic logic
 
             print("Person detected is NOT in a furniture.")
-            move_coords = [person.position_absolute.x, person.position_absolute.y, 0.0]
 
             self.move_to_position(move_coords, print_feedback=print_feedback, feedback_freq=feedback_freq, wait_for_end_of=False)
-
             is_person_radius_reached = False
             while not is_person_radius_reached:
                 robot_pos = self.get_robot_localization()
@@ -2678,10 +2678,11 @@ class RobotStdFunctions():
         else: # if person is in a furniture, it may not be phisically acessible to navigate to the coordinates, because this may be blocked in the map, if so, we navigate the navigation coords of that furniture
             
             print("Person detected is in furniture:" + person.furniture_location)
-            
+            # move to nav_coords of the furniture where the person is, which are accessible in the map
             move_coords = self.get_navigation_coords_from_furniture(person.furniture_location) 
-            self.move_to_position(move_coords, print_feedback=print_feedback, feedback_freq=feedback_freq, wait_for_end_of=False)
-
+            self.move_to_position(move_coords, print_feedback=print_feedback, feedback_freq=feedback_freq, wait_for_end_of=True)
+            # rotate to person position (most furnitures not necessary but for sofa must be done)
+            self.adjust_angle(angle=self.get_rotation_angle_to_map_coords(target_coords=move_coords), wait_for_end_of=True)
 
     def move_to_position_follow_waypoints(self, move_coords = [], print_feedback=True, feedback_freq=1.0, wait_for_end_of=True):
 
