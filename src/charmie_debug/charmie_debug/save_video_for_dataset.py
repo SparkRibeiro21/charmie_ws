ae9891cb4228e932c8e367072cd48c6d5b7be770
Tiago Ribeiro <tiagoribeiro80@gmail.com>
2026-06-22 16:01:38 +0100

    Updated code that creates videos from the cameras for dataset creation optimization

--- src/charmie_debug/charmie_debug/save_video_for_dataset.py
+++ src/charmie_debug/charmie_debug/save_video_for_dataset.py
@@ -3,6 +3,7 @@ import rclpy
 from rclpy.node import Node
 
 from sensor_msgs.msg import Image
+from realsense2_camera_msgs.msg import RGBD
 from cv_bridge import CvBridge
 from datetime import datetime
 from pathlib import Path
@@ -20,73 +21,104 @@ class SaveImagesDatasetNode(Node):
         self.midpath = "charmie_ws/src/charmie_point_cloud/charmie_point_cloud"
         self.complete_path = self.home+'/'+self.midpath+'/test_images/'
 
-        # Intel Realsense Subscribers
-        self.color_image_head_subscriber = self.create_subscription(Image, "/CHARMIE/D455_head/color/image_raw", self.get_color_image_head_callback, 10)
-        # self.aligned_depth_image_head_subscriber = self.create_subscription(Image, "/CHARMIE/D455_head/aligned_depth_to_color/image_raw", self.get_aligned_depth_image_head_callback, 10)
-        self.color_image_hand_subscriber = self.create_subscription(Image, "/CHARMIE/D405_hand/color/image_rect_raw", self.get_color_image_hand_callback, 10)
-        # self.aligned_depth_image_hand_subscriber = self.create_subscription(Image, "/CHARMIE/D405_hand/aligned_depth_to_color/image_raw", self.get_aligned_depth_image_hand_callback, 10)
+        # Intel Realsense Subscribers (RGBD) Head and Hand Cameras
+        self.rgbd_head_subscriber = self.create_subscription(RGBD, "/CHARMIE/D455_head/rgbd", self.get_rgbd_head_callback, 10)
+        self.rgbd_hand_subscriber = self.create_subscription(RGBD, "/CHARMIE/D405_hand/rgbd", self.get_rgbd_hand_callback, 10)
+        # Orbbec Camera (Base)
+        self.color_image_base_subscriber = self.create_subscription(Image, "/camera/color/image_raw", self.get_color_image_base_callback, 10)
+        # self.aligned_depth_image_base_subscriber = self.create_subscription(Image, "/camera/depth/image_raw", self.get_depth_base_image_callback, 10)
 
         self.current_datetime = str(datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
 
+        self.CAM_WIDTH = 868
+        self.CAM_WIDTH_BASE = 640
+        self.CAM_HEIGHT = 480
+
         # Define the codec and create a VideoWriter object
         fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        self.out_head = cv2.VideoWriter(self.home+'/'+self.current_datetime+'_head_charmie.avi', fourcc, 20.0, (1280, 720))
-        self.out_hand = cv2.VideoWriter(self.home+'/'+self.current_datetime+'_hand_charmie.avi', fourcc, 20.0, (1280, 720))
+        self.out_head = cv2.VideoWriter(self.home+'/'+self.current_datetime+'_head_charmie.avi', fourcc, 20.0, (self.CAM_WIDTH,      self.CAM_HEIGHT))
+        self.out_hand = cv2.VideoWriter(self.home+'/'+self.current_datetime+'_hand_charmie.avi', fourcc, 20.0, (self.CAM_WIDTH,      self.CAM_HEIGHT))
+        self.out_base = cv2.VideoWriter(self.home+'/'+self.current_datetime+'_base_charmie.avi', fourcc, 20.0, (self.CAM_WIDTH_BASE, self.CAM_HEIGHT))
                              
         self.br = CvBridge()
-        self.rgb_img_head = Image()
+        self.rgb_head_img = Image()
         # self.depth_img_head = Image()
-        self.rgb_img_hand = Image()
+        self.rgb_hand_img = Image()
         # self.depth_img_hand = Image()
+        self.rgb_base_img = Image()
+        # self.depth_base_img = Image()
 
-        self.is_head_rgb = False
-        # self.is_head_depth = False
-        self.is_hand_rgb = False
-        # self.is_hand_depth = False
+        self.first_rgb_head_image_received = False
+        # self.first_depth_head_image_received = False
+        self.first_rgb_hand_image_received = False
+        # self.first_depth_hand_image_received = False
+        self.first_rgb_base_image_received = False
+        # self.first_depth_base_image_received       = False
 
+    def get_rgbd_head_callback(self, rgbd: RGBD):
+        self.rgb_head_img = rgbd.rgb
+        self.first_rgb_head_image_received = True
+        # self.depth_head_img = rgbd.depth
+        # self.first_depth_head_image_received = True
+        # print("Head (h,w):", rgbd.rgb_camera_info.height, rgbd.rgb_camera_info.width, rgbd.depth_camera_info.height, rgbd.depth_camera_info.width)
 
-    def get_color_image_head_callback(self, img: Image):
-        self.rgb_img_head = img
-        self.is_head_rgb = True
-        
-        current_frame_rgb_head = self.br.imgmsg_to_cv2(self.rgb_img_head, "bgr8")
+        current_frame_rgb_head = self.br.imgmsg_to_cv2(self.rgb_head_img, "bgr8")
         height, width, channels = current_frame_rgb_head.shape   
                 
-        # Make sure all images are the same size (720x1280)
-        if height != 720:
-            current_frame_rgb_head = cv2.resize(current_frame_rgb_head, (1280, 720), interpolation = cv2.INTER_AREA)
+        # Make sure all images are the same size (self.CAM_HEIGHT x self.CAM_WIDTH)
+        if width != self.CAM_WIDTH or height != self.CAM_HEIGHT:
+            current_frame_rgb_head = cv2.resize(current_frame_rgb_head, (self.CAM_WIDTH, self.CAM_HEIGHT), interpolation = cv2.INTER_AREA)
             height, width, channels = current_frame_rgb_head.shape   
             print("Head Colour Image: (Resized)", height, width, channels)    
         else:
             print("Head Colour Image: ", height, width, channels)     
         
         self.out_head.write(current_frame_rgb_head)
-        
         # Display the frame (optional)
         cv2.imshow(self.current_datetime+'_head_charmie.avi', current_frame_rgb_head)
-
         self.check_end_videos()
 
-    def get_color_image_hand_callback(self, img: Image):
-        self.rgb_img_hand = img
-        self.is_hand_rgb = True
-        
-        current_frame_rgb_hand = self.br.imgmsg_to_cv2(self.rgb_img_hand, "bgr8")
+    def get_rgbd_hand_callback(self, rgbd: RGBD):
+        self.rgb_hand_img = rgbd.rgb
+        self.first_rgb_hand_image_received = True
+        # self.depth_hand_img = rgbd.depth
+        # self.first_depth_hand_image_received = True
+        # print("HAND:", rgbd.rgb_camera_info.height, 
+
+        current_frame_rgb_hand = self.br.imgmsg_to_cv2(self.rgb_hand_img, "bgr8")
         height, width, channels = current_frame_rgb_hand.shape   
                 
-        # Make sure all images are the same size (720x1280)
-        if height != 720:
-            current_frame_rgb_hand = cv2.resize(current_frame_rgb_hand, (1280, 720), interpolation = cv2.INTER_AREA)
+        # Make sure all images are the same size (self.CAM_HEIGHT x self.CAM_WIDTH)
+        if width != self.CAM_WIDTH or height != self.CAM_HEIGHT:
+            current_frame_rgb_hand = cv2.resize(current_frame_rgb_hand, (self.CAM_WIDTH, self.CAM_HEIGHT), interpolation = cv2.INTER_AREA)
             height, width, channels = current_frame_rgb_hand.shape   
             print("Hand Colour Image: (Resized)", height, width, channels)    
         else:
             print("Hand Colour Image: ", height, width, channels)     
         
         self.out_hand.write(current_frame_rgb_hand)
-        
         # Display the frame (optional)
         cv2.imshow(self.current_datetime+'_hand_charmie.avi', current_frame_rgb_hand)
+        self.check_end_videos()
 
+    def get_color_image_base_callback(self, img: Image):
+        self.rgb_base_img = img
+        self.first_rgb_base_image_received = True
+
+        current_frame_rgb_base = self.br.imgmsg_to_cv2(self.rgb_base_img, "bgr8")
+        height, width, channels = current_frame_rgb_base.shape   
+                
+        # Make sure all images are the same size (self.CAM_HEIGHT x self.CAM_WIDTH)
+        if width != self.CAM_WIDTH_BASE or height != self.CAM_HEIGHT:
+            current_frame_rgb_base = cv2.resize(current_frame_rgb_base, (self.CAM_WIDTH_BASE, self.CAM_HEIGHT), interpolation = cv2.INTER_AREA)
+            height, width, channels = current_frame_rgb_base.shape   
+            print("Base Colour Image: (Resized)", height, width, channels)    
+        else:
+            print("Base Colour Image: ", height, width, channels)     
+        
+        self.out_base.write(current_frame_rgb_base)
+        # Display the frame (optional)
+        cv2.imshow(self.current_datetime+'_base_charmie.avi', current_frame_rgb_base)
         self.check_end_videos()
 
     def check_end_videos(self):
@@ -97,20 +129,14 @@ class SaveImagesDatasetNode(Node):
             # Release the video capture and writer objects
             self.out_head.release()
             self.out_hand.release()
+            self.out_base.release()
 
             # Close all OpenCV windows
             cv2.destroyAllWindows()
 
             print("Video saved as"+self.current_datetime+"_head_charmie.avi")
             print("Video saved as"+self.current_datetime+"_hand_charmie.avi")
-
-    # def get_aligned_depth_image_head_callback(self, img: Image):
-    #     self.depth_img_head = img
-    #     self.is_head_depth = True
-        
-    # def get_aligned_depth_image_hand_callback(self, img: Image):
-    #     self.depth_img_hand = img
-    #     self.is_hand_depth = True
+            print("Video saved as"+self.current_datetime+"_base_charmie.avi")
 
 def main(args=None):
     rclpy.init(args=args)
