c0bc35d10cd89a20e2c3609fa1c9232abf0c5eff
Tiago Ribeiro <tiagoribeiro80@gmail.com>
2026-07-04 03:48:46 +0100

    commmit

--- src/charmie_std_functions/charmie_std_functions/task_ros2_and_std_functions.py
+++ src/charmie_std_functions/charmie_std_functions/task_ros2_and_std_functions.py
@@ -8490,7 +8490,7 @@ class RobotStdFunctions():
             GRAB_DOOR_ROTATE= -20
             GRAB_DOOR_Y= 10
             grab_door = [0.0, GRAB_DOOR_Y, 0.0, GRAB_DOOR_ROTATE, GRAB_DOOR_Y, 0.0]
-            grab_door_2=[0.0, 0.085*1000, 0.0, 0.0, 0.0, 0.0]
+            grab_door_2=[0.0, 0.080*1000, 0.0, 0.0, 0.0, 0.0]
             grab_door_return=[0.0, -0.075*1000, 0.0, 0.0, 0.0, 0.0]
             release_door = [0.0, -GRAB_DOOR_Y, 0.0, -GRAB_DOOR_ROTATE, -GRAB_DOOR_Y, 0.0]
             self.set_arm(command="adjust_move_tool_line_quick", move_tool_line_pose = grab_door, wait_for_end_of=True)
@@ -8505,10 +8505,10 @@ class RobotStdFunctions():
             #after_release_second = [0.05*1000, 0.1*1000, 0.16*1000, 0.0, 0.0, 0.0]
             after_release_last = [0.25*1000, - 0.12*1000, 0.195*1000, 0.0, -20.0, 0.0]
             self.set_arm(command="adjust_move_tool_line_quick", move_tool_line_pose = after_release_first, wait_for_end_of=True)  
-            self.adjust_omnidirectional_position(dx = 0.02 , dy = - 0.28, wait_for_end_of=True, safety=False)
+            self.adjust_omnidirectional_position(dx = 0.00 , dy = - 0.28, wait_for_end_of=True, safety=False)
             self.set_arm(command="adjust_move_tool_line_quick", move_tool_line_pose = after_release_last, wait_for_end_of=True)
             #CHANGE ARENA 3
-            self.adjust_omnidirectional_position(dx = 0.05 , dy = 0.0, wait_for_end_of=True, safety=False)
+            self.adjust_omnidirectional_position(dx = 0.07 , dy = 0.0, wait_for_end_of=True, safety=False)
             self.set_arm(command="close_gripper", wait_for_end_of=False)
             #self.adjust_omnidirectional_position(dx = -0.05 , dy = 0.0, wait_for_end_of=False, safety=False)
 
